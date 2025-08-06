c9f4476781878b40c21740fef061ff8d8d530b17
LioneWise77 <sebbepebe27@gmail.com>
2025-08-06 23:17:52 +0200

    updating and improving portfolio

--- OneDrive/Dokument/Portfolio Full-stack/My-Portfolio/my-portfolio/src/components/Resume.js
+++ OneDrive/Dokument/Portfolio Full-stack/My-Portfolio/my-portfolio/src/components/Resume.js
@@ -3,23 +3,19 @@ import styles from "../styles/Resume.module.css";
 
 function Resume() {
   return (
-    <section id="resume" className={styles.resume}>
+    <section id="resume" className={`${styles.resume} ${styles.animateResume}`}>
       <h2>My Resume</h2>
-      <p>Download my resume in your preferred language:</p>
+      <p className={styles.subtitle}>
+        Building bridges between code, creativity and purpose.
+      </p>
+
       <div className={styles.buttonContainer}>
         <a
-          href="/ResumeSvenska.pdf"
+          href="/CV-FullStack-Definitive.pdf"
           className={`btn btn-primary ${styles.button}`}
           download
         >
-          Download Resume (Swedish)
-        </a>
-        <a
-          href="/ResumeEnglish.pdf"
-          className={`btn btn-secondary ${styles.button}`}
-          download
-        >
-          Download Resume (English)
+          Download Resume
         </a>
       </div>
     </section>
